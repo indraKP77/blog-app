1695d41eb673e105c3843ed8fea9aaba553d1ffa
Indra Kumar <indravip3@gmail.com>
2024-07-08 22:33:37 +0530

    author image

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,5 +1,6 @@
 'use client'
-import { blog_data } from '@/Assets/assets';
+import { assets, blog_data } from '@/Assets/assets';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 const page = ({params}) => {
@@ -20,9 +21,20 @@ const page = ({params}) => {
         fetchBlogData()
     },[])
 
-  return (
+  return (data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'> 
+        <div className='flex justify-between items-center'>
+            <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
+                Get started<Image src={assets.arrow} alt='' />
+                </button>
+        </div>
+        <div className='text-center my-24'>
+            <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+            <Image src={data.author_img} width={60} height={60} alt='' />
+        </div>
     </div>
+    </>:<></>
   )
 }
 
